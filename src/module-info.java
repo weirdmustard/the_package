16bb2b0bba8730427fcda957cdd87ea38ccef402
weirdmustard <samael.schroeder@mail.de>
2024-04-25 10:55:35 +0200

    create

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module the_package_game {
+}
---
+++ src/the_package_game/Main.java
@@ -0,0 +1,10 @@
+package the_package_game;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
